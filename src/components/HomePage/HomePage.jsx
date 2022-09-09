ec615a6d386679e5bba8981158d0cfbcc6a65230
Anna Ojdana <anna.ojdana@gmail.com>
2022-09-09 15:59:30 +0200

    HomePage structure

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,11 +1,18 @@
+import styles from './HomePage.module.css';
 import React from 'react';
 // import PropTypes from 'prop-types';
 
-const HomePage = props => {
+const HomePage = ({ movies }) => {
+  const { section, heading } = styles;
   return (
-    <div>
-
-    </div>
+    <section className={section}>
+      <h1 className={heading}>Trending today</h1>
+      <ul className={trendy__list}>
+        {movies.map(movie => (
+          <li className={trendy__item}>{movie.name}</li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
