ca0b9d51b75e85bb64234898a820e2082bd19a1a
Anna Ojdana <anna.ojdana@gmail.com>
2022-09-09 14:24:16 +0200

    styles for link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,33 @@
 import styles from './App.module.css';
-import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import React from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import HomePage from './HomePage/HomePage';
 
+const StyledLink = styled(NavLink)`
+  color: black;
 
+  &.active {
+    color: orange;
+  }
+`;
 
 const App = () => {
-  const { container } = styles;
+  const { container, nav, link } = styles;
 
   return (
     <div className={container}>
-
+      <nav className={nav}>
+        <StyledLink className={link} to="/">
+          Home
+        </StyledLink>
+        <StyledLink className={link} to="/movies">
+          Movies
+        </StyledLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 };
